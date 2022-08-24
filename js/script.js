0fa99f963989fa29ca86e15021f76ea9c5314803
Abdelrahman-Ibrahem <79354542+Abdo-112002@users.noreply.github.com>
2022-08-24 10:29:57 -0700

    add

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+
+import translation from './translation.js';
+
+
+// dropDown-menu
+let avatar = document.querySelector('.header__control--avatar'),
+    notification = document.querySelector('.header__control--notification');
+
+dropDownMenu(avatar);
+dropDownMenu(notification);
+function dropDownMenu (selector) {
+    selector.addEventListener('click',() => {
+        let dropDown = selector.querySelector(".dropdown__menu");
+        dropDown.classList.toggle('active');
+        document.querySelectorAll(".dropdown__menu").forEach((item) => {
+            if(item.parentElement !== selector){
+                item.parentElement.querySelector(".dropdown__menu").classList.remove('active');
+            }
+        });
+    });
+};
+
+
+// side bar
+let aside  = document.querySelector(".aside"),
+    openBtn = document.querySelector(".fa-bars");
+    openBtn.addEventListener('click',(e) => {
+        aside.classList.toggle('active');
+        openBtn.style.cssText = 'transform: scale(0);';
+        setTimeout(() => {
+            openBtn.style.cssText = 'transform: scale(1);';
+        }, 100);
+        
+        (e.target.className == 'fa fa-bars') 
+        ? e.target.className = 'fa fa-x' 
+        : e.target.className = 'fa fa-bars';
+    });
+
+
+
+// toggle language
+let toggleLang = document.querySelector('.toggle_lang');
+toggleLang.addEventListener('change',(e) => {
+    changeLang(e.target.value);
+    localStorage.setItem('lang',e.target.value);
+    location.reload();
+});
+
+document.addEventListener('DOMContentLoaded',() => {
+    changeLang(localStorage.getItem('lang'));
+});
+
+function changeLang(selectorLang) {
+    let allElements = document.querySelectorAll('[data-lang]');
+    allElements.forEach((item) => {
+        let key = item.getAttribute('data-lang');
+        item.textContent = translation[selectorLang][key];
+        if(key == 'search') {
+            item.setAttribute('placeholder',translation[selectorLang][key]);
+        }
+    });
+    document.dir = selectorLang === 'en' ? 'ltr' : "rtl";
+    for(let i=0 ; i< toggleLang.options.length ; i++) {
+        if(toggleLang.options[i].value == selectorLang){
+            toggleLang.options[i].selected = true;
+        }
+    }
+}
---
+++ js/translation.js
@@ -0,0 +1,61 @@
+
+
+let translation = {
+
+    en : {
+        english : "English",
+        arabic : "Arabic",
+        '3' : '3',
+        notification : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, ea.",
+        profile : "Profile",
+        setting : "Setting",
+        payments : "Payments",
+        log_out : "Log out",
+        search : "Search",
+
+        dashboard : "Dashboard",
+        analytics : "Analytics",
+        organization : "Organization",
+        employees : "Employees",
+        calendar : "Calendar",
+        reports :"Reports",
+        files : "Files",
+        directory : "Directory",
+        elements : "Elements",
+        more :"More",
+        to_do : "To Do",
+        in_progress : "In Progress",
+        completed : "Completed",
+        issues : "Issues",
+    },
+
+    ar: {
+        english : "انجلزي",
+        arabic : "عربي",
+        '3' : '3',
+        notification : "لديك بعض ارسائل لم تراها حتي الان اضغط للمشاهده",
+        profile : "الصفحه الشخصيه",
+        setting : "الأعدادات",
+        payments : "المدفوعات",
+        log_out : "تسجيل الخروج",
+        search : "بحث",
+
+        dashboard : "لوحة التحكم",
+        analytics : "تحليلات",
+        organization : "المنظمة",
+        employees : "الموظفين",
+        calendar : "تقويم",
+        reports :"التقارير",
+        files : "الملفات",
+        directory : "الدليل",
+        elements : "عناصر",
+        more :"أكثر",
+        to_do : "لكى يفعل",
+        in_progress : "في تَقَدم",
+        completed : "مكتمل",
+        issues : "مشاكل",
+    }
+
+}
+
+export default translation;
